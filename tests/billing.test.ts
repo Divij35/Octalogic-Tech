524ec68ab50e8ca023bfa754b78150d9e671f7fa
Divij <pirankardivij@gmail.com>
2025-06-21 19:51:09 +0530

    Update: billing structure changed for better readability

--- tests/billing.test.ts
+++ tests/billing.test.ts
@@ -4,17 +4,21 @@ import { login } from '../helpers/login';
 test.describe('Billing Information Test', () => {
   test.beforeEach(async ({ page }) => {
     await login(page);
-  });
-
-  test('Update billing name and payment method', async ({ page }) => {
     await page.getByRole('button', { name: 'User' }).click();
     await page.getByRole('link', { name: 'Account' }).click();
     await page.getByRole('tab', { name: 'Billing' }).click();
-
-    await page.getByRole('button', { name: 'Jayvion Simon' }).click();
-    await page.getByRole('button', { name: 'Deja Brady 18605 Thompson' }).click();
-
-    await page.getByRole('button', {name: '**** **** ****'}).click();
-    page.getByRole('dialog').locator('div').filter({ hasText: '**** **** ****' }).nth(1)
   });
+
+  test('Update billing name', async ({ page }) => {
+  await page.getByRole('button', { name: 'Jayvion Simon' }).click();
+  await page.getByRole('button', { name: 'Deja Brady 18605 Thompson' }).click();
+  await expect(page.getByRole('button', { name: 'Deja Brady' })).toBeVisible();
+})
+
+test('Update payment method', async ({page}) =>{
+  await page.getByRole('button', {name: '**** **** ****'}).click();
+  await page.getByRole('dialog').locator('div').filter({ hasText: '**** **** ****' }).nth(1)
+  await expect(page.getByRole('dialog').locator('div').filter({ hasText: '**** **** ****' }).nth(1)).toBeVisible();
 });
+});
+
